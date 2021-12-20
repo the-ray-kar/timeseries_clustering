2399e722fcf4df8ddc32c18f1b090ebd82403763
Akshay Darekar <70949901+the-ray-kar@users.noreply.github.com>
2021-12-20 23:01:38 +0530

    Update tseries_clustering.py

--- tseries_clustering.py
+++ tseries_clustering.py
@@ -2,6 +2,7 @@ from tslearn import clustering
 from tslearn.metrics import cdist_dtw #Used to generate similarity matrix for timeseries
 from tslearn.clustering import TimeSeriesKMeans
 from sklearn.cluster import DBSCAN
+import time
 
 
 class tseries_clusterer:
@@ -10,17 +11,19 @@ class tseries_clusterer:
         self.data = data
         self.distance_matrix = None
     
-
-
     def kmeans(self,clusters=3,max_iter=50,random_state=0):
-        km = TimeSeriesKMeans(n_clusters=clusters,max_iter=max_iter,random_state=random_state).fit(self.data)
-        return km._labels
+        labels = TimeSeriesKMeans(n_clusters=clusters,max_iter=max_iter,random_state=random_state).fit_predict(self.data)
+        return labels
     
     def dbscan(self,eps=0.1,min_neighbours=3):
+        start = time.time()
         self.distance_matrix = cdist_dtw(self.data,self.data) #will take considerable amount of time
-        dbs = DBSCAN(eps=eps,min_samples=min_neighbours,metric="precomputed").fit(self.distance_matrix)
-        return dbs._labels
+        end = time.time()
+        print("Similarity matix precomputed, took ",(end-start),"seconds for data size",len(self.data),"\n and shape",self.data[0].shape)
+        labels = DBSCAN(eps=eps,min_samples=min_neighbours,metric="precomputed").fit_predict(self.distance_matrix)
+        return labels
 
 
+        
 
 
